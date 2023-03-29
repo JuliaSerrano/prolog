586f11222d8ec83cf3584118495e3e12c6732d5c
Julia <jserranoarrese@gmail.com>
2023-03-29 16:40:51 +0200

    Enunciado Practica1

---
+++ Practica1/code.pl
@@ -0,0 +1,3 @@
+:- module(_,_,[]).
+
+author_data('Serrano', 'Arrese', 'Julia', '200119').
--- prueba.pl
+++
@@ -1,9 +0,0 @@
-% Este programa es un ejemplo simple de Ciao Prolog
-% que imprime "Hola, mundo!" en la consola.
-
-:- initialization(main).
-
-main :-
-    write('Hola, mundo!'),
-    nl,
-    halt.
